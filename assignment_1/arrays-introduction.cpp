519188d825fa3a16766ea33b3fb42182418904ea
SatrudhanKumar <107273461+SatrudhanKumar@users.noreply.github.com>
2022-09-21 22:31:21 +0530

    Create arrays-introduction.cpp

---
+++ assignment_1/arrays-introduction.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    
+    //reverse of array
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[n-i-1]<<" ";
+    }
+    
+    return 0;
+}
