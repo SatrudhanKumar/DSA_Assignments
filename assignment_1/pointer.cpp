1773045629c65443c6301c09ff82629f3236ea27
SatrudhanKumar <107273461+SatrudhanKumar@users.noreply.github.com>
2022-09-21 23:33:42 +0530

    Create pointer.cpp

---
+++ assignment_1/pointer.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    int c=a+b;
+    int d=abs(a-b);
+    cout<<c<<endl<<d<<endl;
+    
+     return 0;
+}
