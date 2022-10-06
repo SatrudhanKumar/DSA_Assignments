6b67f6e133ef088cf2dbfd1156aaad019823ddb5
SatrudhanKumar <107273461+SatrudhanKumar@users.noreply.github.com>
2022-10-06 20:34:41 +0530

    Create A Very Big Sum.cpp

---
+++ assignment_3/A Very Big Sum.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() { int n,i; cin>>n; int arr[n];
+
+long long sum=0;
+
+for(i=0; i<n; i++)
+{
+    cin>>arr[i];
+} 
+
+for(i=0; i<n; i++)
+{
+    sum+=arr[i];
+}
+cout<<sum;
+
+return 0;
+}
